2275c33d15d8087ffcdd4742ac371004d88158b9
agustinjob <agus_job@hotmail.com>
2024-08-13 00:11:40 -0600

    change custom hook by a reducer logic

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,17 @@
+import { OrderAction } from "../reducers/order-reducer"
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+    item: MenuItem,
+    dispatch: React.Dispatch<OrderAction>
+}
+
+export default function MenuItem({ item, dispatch }: MenuItemProps) {
+    return (
+        <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+            onClick = {() => dispatch({type:'add-item',payload:{item}}) }>
+            <p>{item.name}</p>
+            <p className="font-black">${item.price}</p>
+        </button>
+    )
+}
